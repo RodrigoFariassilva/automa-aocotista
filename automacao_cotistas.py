cfbfd6c30be85b2d876ede53057df3c0978df94a
Rodrigo Farias <166635869+RodrigoFariassilva@users.noreply.github.com>
2025-10-31 15:33:22 -0300

    Update automacao_cotistas.py

--- automacao_cotistas.py
+++ automacao_cotistas.py
@@ -114,7 +114,7 @@ cotistas = remove_chars_and_terms(cotistas, 'Nome')
 transacoes_files = st.file_uploader("Upload dos Arquivos de Transações (.xls, .xlsx)", type=["xls", "xlsx"], accept_multiple_files=True)
 
 if transacoes_files:
-    cotistas = pd.read_csv(cotistas_file, delimiter=';')
+   cotistas = pd.read_csv("assets/cotistas.csv", delimiter=';')
     cotistas = remove_chars_and_terms(cotistas, 'Nome')
 
     dfs = []
